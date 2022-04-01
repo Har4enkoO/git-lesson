c5e78a48974a6204ae67ed8e1557ee50a6f17018
Har4enkoO <pan4enko48@gmail.com>
2022-04-01 16:59:43 +0300

    add 2 task

--- task2/index.js
+++ task2/index.js
@@ -1,4 +1,15 @@
-function calculate() {
-  //Here is your solution
-  return
+function calculate(N) {
+  if (N < 0) return null;
+
+  let matrix = [];
+  for (let row = 0; row < N; row++) {
+    let cells = [];
+    for (let cell = 0; cell < N; cell++) {
+      let letter = String.fromCharCode(97 + ((row + cell) % 26));
+      cells = [...cells, letter];
+    }
+    matrix = [...matrix, cells.join(" ")];
+  }
+
+  return matrix.join("\n");
 }
