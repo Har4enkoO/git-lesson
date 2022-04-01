c9e97861a8bb43c9fc80c1db993a5590edaf3091
Har4enkoO <pan4enko48@gmail.com>
2022-04-01 16:52:28 +0300

    add 1 task

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,20 @@
-function calculate() {
-  //Here is your solution
-  return
+function calculate(example) {
+  example = example.split(" ");
+
+  let [leftPart, operator, rightPart] = example;
+
+  switch (operator) {
+    case "+":
+      result = leftPart.length + rightPart.length;
+      return ".".repeat(result);
+    case "-":
+      result = leftPart.length - rightPart.length;
+      return ".".repeat(result);
+    case "*":
+      result = leftPart.length * rightPart.length;
+      return ".".repeat(result);
+    case "//":
+      result = Math.trunc(leftPart.length / rightPart.length);
+      return ".".repeat(result);
+  }
 }
